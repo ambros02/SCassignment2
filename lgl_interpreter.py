6256993aeb2bbcb0e369a31631df126bd13f5507
Wassim Mehran <wassim.mehran@uzh.ch>
2023-11-06 13:59:35 +0100

    Update lgl_interpreter.py

--- lgl_interpreter.py
+++ lgl_interpreter.py
@@ -1,1 +1,35 @@
+import sys
+import json
 
+#Multiplikation, Division, Potenz
+#Print statements
+#While loops
+
+OPERATIONS = {
+    func_name.replace("do_",""): func_body
+    for (func_name, func_body) in globals().items()
+    if func_name.startswith("do_")
+}
+
+
+def do(envs,expr):
+    if isinstance(expr,int):
+        return expr
+   
+    assert isinstance(expr,list)
+    assert expr[0] in OPERATIONS, f"Unknown operation {expr[0]}"
+    func = OPERATIONS[expr[0]]
+    return func(envs, expr[1:])
+
+
+def main():
+    assert len(sys.argv) == 2, "Usage: funcs-demo.py filename.gsc"
+    with open(sys.argv[1], "r") as source_file:
+        program = json.load(source_file)
+    assert isinstance(program,list)
+    envs = [{}]
+    result = do(envs,program)
+    print(f"=> {result}")
+
+if __name__ == "__main__":
+    main()

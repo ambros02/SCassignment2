676da366ae229baa298dc516febcc868950c5068
Wassim Mehran <wassim.mehran@uzh.ch>
2023-11-06 17:15:55 +0100

    Update lgl_interpreter.py

--- lgl_interpreter.py
+++ lgl_interpreter.py
@@ -1,35 +1,95 @@
 import sys
 import json
 
-#Multiplikation, Division, Potenz
-#Print statements
-#While loops
+
+def do_setzen(env, args):
+    assert len(args) == 2
+    assert isinstance(args[0], str)
+    var_name = args[0]
+    value = do(env, args[1])
+    env[var_name] = value
+    return value
+
+
+def do_abrufen(env, args):
+    assert len(args) == 1
+    assert isinstance(args[0], str)
+    assert args[0] in env, f"Unknown var {args[0]}"
+    return env[args[0]]
+
+
+# Multiplizieren
+def do_multiplizieren(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    return left * right
+
+
+# Dividieren
+def do_dividieren(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    return left / right
+
+
+# Potenzieren
+def do_potenzieren(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    return left ** right
+
+
+# Print
+def do_print(env, args):
+    assert len(args) > 0
+    print(args)
+
+
+# While
+def do_while(env, args):
+    assert len(args) == 2
+    cond = do(env, args[0])
+    operation = do(env, args[1])
+    while cond:
+        operation()
+
+
+def do_abfolge(env, args):
+    assert len(args) > 0
+    for operation in args:
+        result = do(env, operation)
+        return result
+
 
 OPERATIONS = {
-    func_name.replace("do_",""): func_body
+    func_name.replace("do_", ""): func_body
     for (func_name, func_body) in globals().items()
     if func_name.startswith("do_")
 }
 
 
-def do(envs,expr):
-    if isinstance(expr,int):
+def do(env, expr):
+    if isinstance(expr, int):
         return expr
-   
-    assert isinstance(expr,list)
+
+    assert isinstance(expr, list)
     assert expr[0] in OPERATIONS, f"Unknown operation {expr[0]}"
     func = OPERATIONS[expr[0]]
-    return func(envs, expr[1:])
+    return func(env, expr[1:])
 
 
 def main():
-    assert len(sys.argv) == 2, "Usage: funcs-demo.py filename.gsc"
+    assert len(sys.argv) == 2, "Usage: lgl_interpreter.py filename.gsc"
     with open(sys.argv[1], "r") as source_file:
         program = json.load(source_file)
-    assert isinstance(program,list)
-    envs = [{}]
-    result = do(envs,program)
+    assert isinstance(program, list)
+    env = {}
+    result = do(env, program)
     print(f"=> {result}")
+
 
 if __name__ == "__main__":
     main()

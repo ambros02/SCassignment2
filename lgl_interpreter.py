6569b9d7f696010b8f329929cb5fc1dffaac93d9
Wassim Mehran <wassim.mehran@uzh.ch>
2023-11-09 11:47:18 +0100

    Update lgl_interpreter.py

--- lgl_interpreter.py
+++ lgl_interpreter.py
@@ -126,26 +126,26 @@ class LGL_Interpreter:
             del self.dictionaries[line[2]]
         return None
 
-    def interpret_multiplizieren(self, line: list):
+        def interpret_multiplizieren(self, line: list):
         """This method allows to multiply"""
-        assert len(line) == 2
-        left = self.interpret(line[0])
-        right = self.interpret(line[1])
-        return print(left * right)
+        assert len(line) == 3
+        left = self.interpret(line[1])
+        right = self.interpret(line[2])
+        return left * right
 
     def interpret_dividieren(self, line: list):
         """This method allows to divide"""
-        assert len(line) == 2
-        left = self.interpret(line[0])
-        right = self.interpret(line[1])
-        return print(left / right)
+        assert len(line) == 3
+        left = self.interpret(line[1])
+        right = self.interpret(line[2])
+        return left / right
 
     def interpret_potenzieren(self, line: list):
         """This method allows to calculate the power of a basis"""
-        assert len(line) == 2
-        left = self.interpret(line[0])
-        right = self.interpret(line[1])
-        return print(left ** right)
+        assert len(line) == 3
+        left = self.interpret(line[1])
+        right = self.interpret(line[2])
+        return left ** right
 
     def interpret_print(self, line: list):
         """This method allows to use the print statement"""
@@ -154,9 +154,9 @@ class LGL_Interpreter:
 
     def interpret_while(self, line: list):
         """This method allows to use while loops, STILL WORKING ON BETTER SOLUTION"""
-        assert len(line) == 2
-        cond = line[0]
-        operation = line[1]
+        assert len(line) == 3
+        cond = line[1]
+        operation = line[2]
         if isinstance(cond, bool):
             while cond:
                 exec(operation)
@@ -185,90 +185,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-
-"""import sys
-import json
-
-
-def do_abfolge(env, args):
-    assert len(args) > 0
-    for operation in args:
-        result = do(env, operation)
-    return result
-
-
-# Multiplizieren
-def do_multiplizieren(env, args):
-    assert len(args) == 2
-    left = do(env, args[0])
-    right = do(env, args[1])
-    return print(left * right)
-
-
-# Dividieren
-def do_dividieren(env, args):
-    assert len(args) == 2
-    left = do(env, args[0])
-    right = do(env, args[1])
-    return print(left / right)
-
-
-# Potenzieren
-def do_potenzieren(env, args):
-    assert len(args) == 2
-    left = do(env, args[0])
-    right = do(env, args[1])
-    return print(left ** right)
-
-
-# Print
-def do_print(env, args):
-    assert len(args) > 0
-    print(args)
-
-
-# While
-def do_while(env, args):
-    assert len(args) == 2
-    cond = args[0]
-    operation = args[1]
-    if isinstance(cond, bool):
-        while cond:
-            exec(operation)
-    elif isinstance(cond, int):
-        count = 0
-        while count < cond:
-            exec(operation)
-            count += 1
-
-
-OPERATIONS = {
-    func_name.replace("do_", ""): func_body
-    for (func_name, func_body) in globals().items()
-    if func_name.startswith("do_")
-}
-
-
-def do(env, expr):
-    if isinstance(expr, int):
-        return expr
-
-    assert isinstance(expr, list)
-    assert expr[0] in OPERATIONS, f"Unknown operation {expr[0]}"
-    func = OPERATIONS[expr[0]]
-    return func(env, expr[1:])
-
-
-def main():
-    assert len(sys.argv) == 2, "Usage: lgl_interpreter.py filename.gsc"
-    with open(sys.argv[1], "r") as source_file:
-        program = json.load(source_file)
-    assert isinstance(program, list)
-    env = {}
-    result = do(env, program)
-    print(f"=> {result}")
-
-
-if __name__ == "__main__":
-    main()"""
+    

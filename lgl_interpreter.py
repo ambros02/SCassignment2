34f96a8872de12d1d76172de1786df551fc322f8
Ambros Eberhard <ambros.eberhard@gmx.ch>
2023-11-11 23:24:23 +0100

    removed clean functions and rely fully on interpreter

--- lgl_interpreter.py
+++ lgl_interpreter.py
@@ -64,28 +64,6 @@ class LGL_Interpreter:
             return [value]
         return value
 
-    def call_clean(self,line:list,low_limit:int = 1, high_limit:int = None) -> list:
-        
-        if not isinstance(line,list):
-            return line
-        if len(line) == 0:
-            return line
-
-        if all([not isinstance(instr,list) for instr in line]):
-            return self.clean([line],low_limit,high_limit)[0]
-
-        return self.clean(line,low_limit,high_limit)
-
-    def clean(self, line:list,low_limit:int = 1,high_limit:int = None) -> list:
-        """Interpret the nested functions in the lines, define low and high limit for the range of elements which should be interpreted if possible"""
-        if not isinstance(line,list):
-            return line
-        
-        for index,value in enumerate(line[low_limit:high_limit]):
-            if isinstance(value,list) and value[0] in [name.replace("interpret_","") for name in dir(self.__class__) if name.startswith("interpret_")]:
-                line[index+low_limit] = self.interpret(value)
-
-        return line
 
 
     def interpret(self, instruction:list) -> None:
@@ -94,8 +72,8 @@ class LGL_Interpreter:
         if isinstance(instruction,(int,str,type(None))):
             return instruction
         
-        if isinstance(instruction[0],list):
-            instruction = self.call_clean(instruction,0,1)
+        if isinstance(instruction[0],list): 
+            instruction[0] = self.interpret(instruction[0])
         assert "interpret_" + str(instruction[0]) in dir(self.__class__), f"Unknown operation: {instruction[0]}"
         #get the name of the method to execute then get the actual method
         method_name = [method for method in dir(self.__class__) if method.replace("interpret_","") == instruction[0]][0]
@@ -105,10 +83,9 @@ class LGL_Interpreter:
     def interpret_hoch(self, line:list):
         """this method is for calculating powers"""
         assert len(line) == 3, "bad usage of hoch try: ['hoch',<base>,<power>]"
-        line = self.call_clean(line)
-        for num in line[1:]:
-            assert isinstance(num,(int,float)), "bad usage of hoch, the base and the power need to be ints or floats"
-        return line[1]**line[2]
+        base = self.interpret(line[1])
+        power = self.interpret(line[2])
+        return base ** power
 
     def interpret_pi(self,line:list) -> float:
         assert len(line) == 1, "bad usage of pi: try ['pi']"
@@ -116,26 +93,23 @@ class LGL_Interpreter:
     
     def interpret_addieren(self, line:list):
         assert len(line) >= 3, "bad usage of interpret addieren, need at least 2 arguments"
-        line = self.call_clean(line)
-        result = line[1]
-        for value in line[2:]:
-            result += value
+        result = 0
+        for number in line[1:]:
+            result += self.interpret(number)
         return result
 
     def interpret_multiplizieren(self, line:list):
         assert len(line) == 3, "bad usage of multiplizieren try: ['multiplizieren',<value1>,<value2>]"
-        line = self.call_clean(line)
-        for num in line[1:]:
-            assert isinstance(num,(int,float)), "bad usage of multiplizieren, values need to be int or float"
-        return line[1]*line[2]
+        
+        left = self.interpret(line[1])
+        right = self.interpret(line[2])
+        return left * right
     
     def interpret_dividieren(self, line:list):
         assert len(line) == 3, "bad usage of dividieren try: ['dividieren',<value1>,<value2>]"
-        line = self.call_clean(line)
-        for num in line[1:]:
-            assert isinstance(num,(int,float)), "bad usage of division, values need to be int or float"
-        assert line[2] != 0, "bad usage of division: division by 0 is not allowed"
-        return line[1]/line[2]
+        dividend = self.interpret(line[1])
+        divisor = self.interpret(line[2])
+        return dividend/divisor
 
 
 
@@ -160,14 +134,17 @@ class LGL_Interpreter:
         """This method sets a value to a key in a dictionary. If the dictionary does not exist an error is thrown. If the key already exists the value is overwriten"""
 
         assert len(line) == 4, "bad usage of dictionary setzen: Try ['dictionary_setzen','<name>','<key:str/int>','<value>']"
-        line = self.call_clean(line)
+
+        name = self.interpret(line[1])
+        key = self.interpret(line[2])
+        value = self.interpret(line[3])
 
         try:
-            new_dict = self.environment_get(line[1])
+            name_dict = self.environment_get(name)
         except Exception:
-            raise Exception (f'the dictionary {line[1]} does not exist')
-        assert isinstance(line[2],(int,str)), "the key of the dictionary needs to be a string or an int"
-        new_dict[line[2]] = line[3]
+            raise Exception (f'the dictionary {name} does not exist')
+        assert isinstance(key,(int,str)), "the key of the dictionary needs to be a string or an int"
+        name_dict[key] = value
         
         return None
 
@@ -175,18 +152,20 @@ class LGL_Interpreter:
         """This method allows the user to find values in the dictionary by a specified key. If the name or key does not exist an error is thrown, otherwhise the value is returned"""
 
         assert len(line) == 3, "bad usage of dictionary finden: Try ['dictionary_finden','<name>','<key:str/int>']"
-        line = self.call_clean(line)
+
+        name = self.interpret(line[1])
+        key = self.interpret(line[2])
         #assert line[1] in self.environment.keys(), "the dictionary of which you want to find values does not exist"
         #assert line[2] in self.environment[line[1]].keys(), f"the key {line[2]} does not exist in dictionary {line[1]}"
 
         #value = self.environment[line[1]][line[2]]
 
         try:
-            value = self.environment_get(line[1])
-            assert isinstance(value,dict), f"the variable {line[1]} is not a dictionary"
+            value = self.environment_get(name)
+            assert isinstance(value,dict), f"the variable {name} is not a dictionary"
             return value[line[2]]
         except LookupError:
-            raise Exception (f'the dictionary {line[1]} or the key {line[2]} does not exist')
+            raise Exception (f'the dictionary {name} or the key {key} does not exist')
         #return value
 
 
@@ -196,24 +175,24 @@ class LGL_Interpreter:
 
         assert len(line) == 3, "bad usage of dictionary verbinden: Try ['dictionary_verbinden','<name>','<name>']"
 
-        line = self.call_clean(line)
+        name_first = self.interpret(line[1])
+        name_second = self.interpret(line[2])
 
-        for name in line[1:]:
-            assert(isinstance(name,str)), "names of dictionaries must be strings"
+        
+        assert(isinstance(name_first,str)), "names of dictionaries must be strings"
+        assert(isinstance(name_second,str)), "names of dictionaries must be strings"
         #for name in line[1:2]:
         #    assert name in self.environment.keys(), f"the dictionary {name} does not exist"
 
         dictionaries = []
         try:
-            for index in range(1,3):
-                dictionaries.append(self.environment_get(line[index]))
+            dictionaries.append(self.environment_get(name_first))
+            dictionaries.append(self.environment_get(name_second))
         except LookupError:
-            raise Exception (f'the dictionary {line[index]} does not exist')
+            raise Exception (f'the dictionary you want to merge does not exist does not exist')
 
-        for key,value in dictionaries[1].items():
-            if key not in dictionaries[0].keys():
-                dictionaries[0][key] = value
-        return dictionaries[0]
+
+        return dictionaries[1] | dictionaries[0]
     
         
  
@@ -222,10 +201,13 @@ class LGL_Interpreter:
         """this method allows to store variables into the self.variables dictionary"""
         assert len(line) == 3, "bad usage of variable setzen try: ['variable_setzen','<name:str>',<value>]"
         assert isinstance(line[1],str), "variable name needs to be a string"
-        line = self.call_clean(line)
+        
+        name = self.interpret(line[1])
+        value = self.interpret(line[2])
+
         #assert not isinstance(line[2],(list,dict)), "to store lists and dictionaries use dictionary/liste_erstellen"
         #self.environment[-1][line[1]] = line[2]
-        self.environment_set(line[1],line[2])
+        self.environment_set(name,value)
 
         return None
 
@@ -233,19 +215,19 @@ class LGL_Interpreter:
     def interpret_variable_holen(self, line:list):
         """this method allows to access stored variables"""
         assert len(line) == 2, "bad usage of variable_holen try: ['variable_holen','<name:str>']"
-        line = self.call_clean(line)
-        assert isinstance(line[1],str), "variable name needs to be a string"
+        name = self.interpret(line[1])
+        assert isinstance(name,str), "variable name needs to be a string"
         try:
-            return self.environment_get(line[1])
+            return self.environment_get(name)
         except Exception as e:
-            raise Exception (f'the variable {line[1]} does not exist')
+            raise Exception (f'the variable {name} does not exist')
 
         #assert line[1] in self.environment.keys()
         #return self.environment[line[1]]
     
     def interpret_funktion_erstellen(self, line:list) -> list:
         """this method allows to create functions"""
-        assert len(line) == 3, "bad usage of funktion_erstellen try: ['funktion_erstellen',[<arg1>,<arg2>],[<code>]]"
+        assert len(line) == 3, "bad usage of funktion_erstellen try: ['funktion_erstellen',[<para1>,<para2>],[<code>]]"
         assert isinstance(line[1],list), "parameter names for a function must be given in a list"
         for para in line[1]:
             assert isinstance(para,str), "name of parameters must be strings"
@@ -257,24 +239,26 @@ class LGL_Interpreter:
     def interpret_funktion_aufrufen(self, line:list):
         """this method allows to call functions by their name"""
         assert len(line) == 3, "bad usage of funktion_aufrufen try: ['funktion_aufrufen',<name:str>,[<arguments>]]"
-        line = self.call_clean(line,1,2)
-        assert isinstance(line[1],str), "the name of the function must be a string"
+        assert isinstance(line[2],list), "the arguments for funktion aufrufen where not passed in a list"
+        name = self.interpret(line[1])
+        arguments = []
+        for arg in line[2]:
+            arguments.append(self.interpret(arg))
+        assert isinstance(name,str), "the name of the function must be a string"
         #assert line[1] in self.environment.keys(), "this function does not exist"
         try:
-            func = deepcopy(self.environment_get(line[1]))
+            func = deepcopy(self.environment_get(name))
             assert func[0] == "funktion"
         except LookupError:
-            raise NotImplementedError (f'the function {line[1]} does not exist')
+            raise NotImplementedError (f'the function {name} does not exist')
         except Exception:
-            raise TypeError(f'the function specified: {line[1]} is not a function')
+            raise TypeError(f'the function specified: {name} is not a function')
         
-        assert isinstance(line[2],list), "the arguments for funktion aufrufen where not passed in a list"
-        line[2] = self.call_clean(line[2],0,None)
-        assert len(self.list_transform(line[2])) == len(func[1]), f"the function: {line[1]} must be called with {len(func[1])} arguments but you specified {len(line[2])}"
+        assert len(arguments) == len(func[1]), f"the function: {name} must be called with {len(func[1])} arguments but you specified {len(arguments)}"
         #assert len(line[2]) == len(self.environment[line[1]][1]), f"you tried to call the function {line[1]} with a wrong amount of parameters"
 
 
-        local_env = dict(zip(func[1],self.list_transform(line[2])))
+        local_env = dict(zip(func[1],arguments))
         self.environment.append(local_env)
         result = self.interpret(func[2])
         self.environment.pop(-1)
@@ -283,66 +267,62 @@ class LGL_Interpreter:
     def interpret_retournieren(self, line:list):
         """use this method to return in functions"""
         assert len(line) == 2, "bad usage of retournieren try: ['retournieren',<value>]"
-        line = self.call_clean(line)
-        return line[1]
+        value = self.interpret(line[1])
+        return value
     
     def interpret_klasse_erstellen(self, line:list) -> None:
         """method to create a new class"""
+
         assert len(line) in (3,4), "bad usage of klasse_erstellen try: ['klasse_erstellen',<name:str>,[[<name>,<funktion:name>],[<name>,<funktion>]],'<parent>=None']"
-        line = self.call_clean(line,1,2)
-        assert isinstance(line[1],str), "the name of a class must be a string"
-        assert isinstance(line[2],list) and line[2] != [], f"please define proper methods for class {line[1]}"
+        name = self.interpret(line[1])
+        assert isinstance(name,str), "the name of a class must be a string"
+        assert not self.environment_inspect("class_"+name), f'the class {name} does already exist'
 
+        assemble = {"name":name}
 
-        assert not self.environment_inspect("class_"+line[1]), f'the class {line[1]} does already exist'
+        assert isinstance(line[2],list) and line[2] != [], f"please define proper methods for class {name}"
+
+        for method in line[2]:
+            assert len(method) == 2, f"define methods for class {name} properly try: [[<name>,<funktion:name>],[<name>,<funktion>]]"
+            method_name = self.interpret(method[0])
+            func_name = self.interpret(method[1])
+            assert self.environment_inspect(func_name), f"the function {func_name} is not implemented"
+            assemble[method_name] = func_name
 
 
         if len(line) == 3:
-            line.append(None)
-        line = self.call_clean(line,3)
-        assert isinstance(line[3],(str,type(None))), f"the parent name has to be a string or None"
+            parent = None
+        else:
+            parent = self.interpret(line[3])
+            assert isinstance(parent,(str,type(None))), f"the parent name has to be a string or None"
+            assert self.environment_inspect("class_" + parent), f"the parent {parent} does not exist"
 
-        if line[3] != None:
-            try:
-                parent = self.environment_get("class_"+line[3])
-            except LookupError:
-                raise Exception (f'the parent {line[3]} does not exist')
+        assemble["parent"] = parent      
 
-        assemble = {"name":line[1],}
-
-        
-        for method in line[2]:
-                    assert len(method) == 2, f"incorrect methods specified for class {line[1]}"
-                    try:
-                        func = self.environment_get(method[1])
-                        assemble[method[0]] = method[1]
-                        pass
-                    except LookupError:
-                        raise NotImplementedError (f'the function {method[1]} is not implemented')
-        assemble["parent"] = line[3]       
-
-        self.environment_set("class_"+line[1], assemble)
+        self.environment_set("class_" + name, assemble)
         return None
 
     def interpret_objekt_instanzieren(self,line:list) -> dict:
         """instantiate objects of a class by using the standard neu->dict method"""
         assert len(line) == 3, "bad usage of objekt_instanzieren try: ['objekt_instanzieren',<class>,[<arg>,<arg>]]"
-        line = self.call_clean(line,1,2)
-        assert isinstance(line[1],str), "objekt instanzieren: the name of a class must be a string"
-        assert self.environment_inspect("class_"+line[1]), f"the class {line[1]} does not exist"
+        
+        class_name = self.interpret(line[1])
+        assert isinstance(class_name,str), "objekt instanzieren: the name of a class must be a string"
+        assert self.environment_inspect("class_" + class_name), f"the class {class_name} does not exist"
+
         assert isinstance(line[2],list), f"the argumentss for an object instantiation must be given in a list"
-        assemble = {"class":"class_"+line[1]}
+        arguments = []
+        for arg in line[2]:
+            arguments.append(self.interpret(arg))
+
+        assemble = {"class":"class_" + class_name}
         try:
             function_name = self.find_method(assemble["class"],"neu")
-            object_variables = self.interpret_funktion_aufrufen(["funktion_aufrufen",function_name,line[2]])
-            assert isinstance(object_variables,dict), f"the class {line[1]} did not implement neu to return a dictionary"
+            object_variables = self.interpret_funktion_aufrufen(["funktion_aufrufen",function_name,arguments])
+            assert isinstance(object_variables,dict), f"the class {class_name} did not implement neu to return a dictionary"
             assemble |= object_variables
         except Exception:
             pass
-        """ if "neu" in self.environment_get(assemble["class"]).keys():
-            object_variables = self.interpret_funktion_aufrufen(["funktion_aufrufen",self.environment_get(assemble["class"])["neu"],line[2]])
-            assert isinstance(object_variables,dict), f"the class {line[1]} did not implement neu to return a dictionary"
-            assemble |= object_variables """
         
         return assemble
         
@@ -362,19 +342,21 @@ class LGL_Interpreter:
     def interpret_objekt_methode(self, line:list):
         """lets objects use their methods"""
         assert len(line) == 4, "bad usage ob objekt_methode try: ['objekt_methode',<name(object)>,<name(method)>,[<arg>,<arg>]]"
-        line = self.call_clean(line,1,3)
-        for name in line[1:3]:
-            assert isinstance(name,str), "the name of an object/method must be a string"
+
+        object_name = self.interpret(line[1])
+        assert isinstance(object_name,str), "the name of an object must be a string"
+        assert self.environment_inspect(object_name), f"the object {object_name} does not exist"
+
+        method_name = self.interpret(line[2])
+        assert isinstance(method_name,str), "the name of a method must be a string"
+
+        arguments = []
         assert isinstance(line[3],list), "the arguments for a method must be given in a list"
-        for index,arg in enumerate(line[3]):
-            line[3][index] = self.call_clean(arg)
-        try:
-            obj = self.environment_get(line[1])
-        except Exception:
-            raise Exception (f'the object {line[1]} does not exist')
-        
-        meth = self.find_method(obj["class"],line[2])
-        return self.interpret_funktion_aufrufen(["funktion_aufrufen",meth,line[3]])
+        for arg in line[3]:
+            arguments.append(self.interpret(arg))
+
+        func_name = self.find_method(self.environment_get(object_name)["class"],method_name)
+        return self.interpret_funktion_aufrufen(["funktion_aufrufen",func_name,arguments])
     
     def interpret_klasse_methode(self, line:list):
         """lets classes use their methods"""
@@ -398,9 +380,7 @@ def main() -> None:
     german_interpreter = LGL_Interpreter(source_lines)
     german_interpreter.run()
 
-    for x,y in german_interpreter.environment[0].items():
-        print(f"{x} \t", end="")
-        print(y)
+
 
 if __name__ == "__main__":
     main()
